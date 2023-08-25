e5ea068e132246ad367fbd9b320bc001147f6eb5
Alvin Lee <alvin@ootbdev.com>
2023-08-25 11:25:45 -0700

    Implements database and redis functions

--- db.js
+++ db.js
@@ -1,13 +1,28 @@
+require('dotenv').config()
+const faker = require('faker')
+
+const { Client } = require('pg')
+const client = new Client()
+client.connect()
+
 const getAll = async () => {
+  const result = await client.query('select name, email from users')
+  return result.rows
 }
 
 const insert = async () => {
+  const result = await client.query('insert into users(name, email) values($1, $2)', 
+                                    [`${faker.name.firstName()} ${faker.name.lastName()}`, faker.internet.email()])
+  return result.rows
 }
 
-const deleteRecords = async () => {
+const deleteRecords = async (count = 1) => {
+  await client.query('delete from users where id in (select id from users limit $1)', [count])
 }
 
 const getCount = async () => {
+  const result = await client.query('select count(*) as count from users')
+  return result.rows[0]['count']
 }
 
 module.exports = {
--- redis.js
+++ redis.js
@@ -1,13 +1,34 @@
+require('dotenv').config()
+const faker = require('faker')
+const { createClient } = require('redis')
+const client = createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+})
+
+client.on('error', err => console.log('Redis Client Error', err));
+client.connect()
+
+const HASH_NAME = 'ip_addresses'
+
 const getAll = async () => {
+  const result = await client.hGetAll(HASH_NAME)
+  return result
 }
 
 const insert = async () => {
+  await client.HSET(HASH_NAME, faker.datatype.uuid(), faker.internet.ip())
 }
 
-const deleteRecords = async () => {
+const deleteRecords = async (count = 1) => {
+  const keys = await client.hKeys(HASH_NAME)
+  for (let i = 0; i < count && i < keys.length; i++) {
+    await client.hDel(HASH_NAME, keys[i])
+  }
 }
 
 const getCount = async () => {
+  const result = await client.hKeys(HASH_NAME)
+  return result.length
 }
 
 module.exports = {
