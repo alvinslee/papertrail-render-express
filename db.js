b4f3600c07f342acf5b5d6cb6ba16458f264ab1a
Alvin Lee <alvin@ootbdev.com>
2023-08-31 12:39:34 -0700

    Database connect and end

--- db.js
+++ db.js
@@ -2,31 +2,36 @@ require('dotenv').config()
 const faker = require('faker')
 
 const { Client } = require('pg')
-const client = new Client()
-
-try {
-  client.connect()
-} catch (err) {
-  console.log(err)
-}
 
 const getAll = async () => {
+  const client = new Client()
+  client.connect()
   const result = await client.query('select name, email from users')
+  client.end()
   return result.rows
 }
 
 const insert = async () => {
+  const client = new Client()
+  client.connect()
   const result = await client.query('insert into users(name, email) values($1, $2)', 
                                     [`${faker.name.firstName()} ${faker.name.lastName()}`, faker.internet.email()])
+  client.end()
   return result.rows
 }
 
 const deleteRecords = async (count = 1) => {
+  const client = new Client()
+  client.connect()
   await client.query('delete from users where id in (select id from users limit $1)', [count])
+  client.end()
 }
 
 const getCount = async () => {
+  const client = new Client()
+  client.connect()
   const result = await client.query('select count(*) as count from users')
+  client.end()
   return result.rows[0]['count']
 }
 
